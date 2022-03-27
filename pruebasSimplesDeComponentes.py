8513b163a1336cd54bcd66b2a980f97e9313fe85
Erick Infante <vce05594@innovaccion.mx>
2022-03-27 15:43:15 -0600

    Update pruebasSimplesDeComponentes.py
    Se probo el almacenamiento de trazas sin archivos extra

--- pruebasSimplesDeComponentes.py
+++ pruebasSimplesDeComponentes.py
@@ -1,13 +1,30 @@
 lst = ['A', 'E', 'F']
 lst2 = ['A', 'E', 'F', 'C', 'B']
-sourceFile = open('traza.txt', 'w')
-print(lst2, file = sourceFile, end = '')
-sourceFile.close()
-sourceFile= open("traza.txt", "r")
-s=sourceFile.readline()
-sourceFile.close()
-s2="'"+s+"'"
-print(s2)
+listaContarTrazas=list()
+
+nlist=list()
+nlist.append(lst)
+nlist.append(1)
+listaContarTrazas.append(nlist)
+nlist=list()
+nlist.append(lst2)
+nlist.append(21)
+listaContarTrazas.append(nlist)
+# sourceFile = open('traza.txt', 'w')
+# print(lst2, file = sourceFile, end = '')
+# sourceFile.close()
+# sourceFile= open("traza.txt", "r")
+# s=sourceFile.readline()
+# sourceFile.close()
+# s2="'"+s+"'"
+print(listaContarTrazas)
+ban=1
+for x in listaContarTrazas:
+    print(x[0])
+    if lst2 == x[0]:
+        ban=0
+        print("si esta")
+        break
 # print(list1)
 # str2="'"+print(list1)+"'"
 # print(str2)
