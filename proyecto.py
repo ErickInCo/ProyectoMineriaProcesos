120a6d0915557f469bd1c91a58ec40142ed433cd
Erick Infante <vce05594@innovaccion.mx>
2022-04-03 14:10:44 -0500

    Add BPMN generation

--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,7 @@
 # data
 from audioop import reverse
 import pandas as pd
+import pm4py
 from pm4py.objects.conversion.log import converter as log_converter
 from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.log.exporter.xes import exporter as xes_exporter
@@ -130,6 +131,8 @@ print("IVC= "+str(varianza))
 
 ########## Fin de Lectura de trazas ###################
 
-
+process_tree = pm4py.discover_tree_inductive(log)
+bpmn_model = pm4py.convert_to_bpmn(process_tree)
+pm4py.view_bpmn(bpmn_model)
 
 
